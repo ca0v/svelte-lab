efa6f8a6992708b4a51994d9f7c74bf64e9ce724
Corey Alix <coreyalix@gmail.com>
2022-12-28 13:25:34 -0500

    Defines polygon instead of rectange

--- src/store/svg.ts
+++ src/store/svg.ts
@@ -3,7 +3,11 @@ import { writable } from "svelte/store"
 export const svgClipPaths = writable<Array<{ id: string, body: string }>>([])
 
 export function injectRect(id: string, x: number, y: number, width: number, height: number) {
-    const d = `M${x},${y}h${width}v${height}h-${width}z`
+    const left = x;
+    const top = y;
+    const right = x + width;
+    const bottom = y + height;
+    const d = `M${left},${top}L${right},${top}L${right},${bottom}L${left},${bottom}L${left},${top}z`
     const body = `<path d="${d}"/>`
     svgClipPaths.update(p => [...p.filter(p => p.id != id), { id, body }]);
     return id;
